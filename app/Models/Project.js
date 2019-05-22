e2a4cea04cfd8cb4fae5a7743f7321c20498aa6a
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-21 21:13:13 -0400

    Creating CRUD and REST API Relations in AdonisJS

--- app/Models/Project.js
+++ app/Models/Project.js
@@ -2,6 +2,14 @@
 
 const Model = use("Model");
 
-class Project extends Model {}
+class Project extends Model {
+    user (){
+        return this.belongsTo("App/Models/User")
+    }
+
+    tasks () {
+        return this.hasMany("App/Models/Task")
+    }
+}
 
 module.exports = Project;
--- app/Models/Task.js
+++ app/Models/Task.js
@@ -2,6 +2,18 @@
 
 const Model = use("Model");
 
-class Task extends Model {}
+class Task extends Model {
+    project() {
+        return this.belongsTo("App/Models/Project");
+    }
+
+    user() {
+        return this.belongsTo("App/Models/Project");
+    }
+
+    user() {
+        return this.belongsTo("App/Models/File");
+    }
+}
 
 module.exports = Task;
